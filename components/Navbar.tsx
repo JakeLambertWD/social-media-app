3497d01a43cac14f070aba9006f61cc0e0f78dd8
JakeLambertWD <jakelambert1@hotmail.co.uk>
2022-08-04 11:15:20 +0100

    Logo added to Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+
+import Logo from '../utils/tiktik-logo.png';
+
+export const Navbar = () => {
+	return (
+		<div className='flex justify-between items-center border-b-2 border-gray-200 py-2 px-4 w-full '>
+			<Link href='/'>
+				<div className='w=[100px] md:w=[130px] md:h-[30px] h-[38px]'>
+					<Image className='cursor-pointer' src={Logo} alt='TikTik' layout='responsive' />
+				</div>
+			</Link>
+		</div>
+	);
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,12 @@ import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
 
 import '../styles/globals.css';
+import { Sidebar } from '../components/Sidebar';
+import { Navbar } from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [isSSR, setIsSSR] = useState(true);
+
 	useEffect(() => {
 		setIsSSR(false);
 	}, []);
@@ -13,9 +16,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 	return (
 		<div>
-			NavBar
+			<Navbar />
 			<div className='flex gap-6 md:gap-20'>
-				<div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>Sidebar</div>
+				<div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+					<Sidebar />
+				</div>
 			</div>
 			<div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
 				<Component {...pageProps} />
